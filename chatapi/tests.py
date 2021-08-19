1bb6313efe763d6d0309bb5c14498c7c6779c939
oleksii_esaulov <oleksii_esaulov@epam.com>
2021-08-19 07:46:10 +0300

    modified tests

--- chatapi/tests.py
+++ chatapi/tests.py
@@ -1,10 +1,16 @@
 import json
+from rest_framework.test import APITestCase
 
-from django.test import TestCase
 
-
-class URLTests(TestCase):
+class URLTests(APITestCase):
     number = 0
+
+    def postmessage(self):
+        data = {
+            "email": "string@gmail.com",
+            "text": "string"
+        }
+        return self.client.post('/api/single/', format='json', data=data)
 
     def test_list_messages(self):
         response = self.client.get('/api/list/0/', format='json')
@@ -16,22 +22,27 @@ class URLTests(TestCase):
             "text": "string"
         }
         response = self.client.post('/api/single/', format='json', data=data)
-        URLTests.number = response.data['id']
-        print(URLTests.number)
+
         self.assertEqual(response.status_code, 201)
 
     def test_single_update_message(self):
+        response = self.postmessage()
+        URLTests.number = response.data['id']
+
         data = json.dumps({
             "email": "string@gmail.com",
             "text": "string1"
         })
         headers = {"accept: application/json"}
-        response = self.client.patch('/api/single/1/', data, headers=headers, content_type='application/json')
-        print(response)
+        response = self.client.patch(f'/api/single/{URLTests.number}/', data=data,
+                                     headers=headers, content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
     def test_single_delete_message(self):
-        response = self.client.delete('/api/single/1/', format='json')
+        response = self.postmessage()
+        URLTests.number = response.data['id']
+
+        response = self.client.delete(f'/api/single/{URLTests.number}/', format='json')
         self.assertEqual(response.status_code, 204)
 
 
